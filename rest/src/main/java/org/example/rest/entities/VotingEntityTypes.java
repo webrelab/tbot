4e227303864288f9c71964b3250fb8205dc8eecd
Александр Молокович <molokovich.a.a@sberbank.ru>
2021-03-06 10:37:15 +0700

    TBOT-1: feature мелкие правки

--- rest/src/main/java/org/example/rest/entities/VotingEntityTypes.java
+++ rest/src/main/java/org/example/rest/entities/VotingEntityTypes.java
@@ -1,7 +1,8 @@
 package org.example.rest.entities;
 
 public enum VotingEntityTypes {
-    ACTION,
-    CITY_GOAL,
-    ROLE_REQUEST
+    ACTION, // Голосование за проведение акции
+    CITY_GOAL, // Голосование за городские цели
+    ACTION_GOAL, // Голосование за цели акции
+    ROLE_REQUEST // Голосование за выдачу роли
 }
